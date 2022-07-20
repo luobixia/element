3d299fb329eaa85886c72f971de3967a805f1ebf
luobixia <luobixia@cmcm.com>
2022-07-20 19:23:03 +0800

    feat: pagintion component notes

--- packages/pagination/src/pager.vue
+++ packages/pagination/src/pager.vue
@@ -4,29 +4,40 @@
       :class="{ active: currentPage === 1, disabled }"
       v-if="pageCount > 0"
       class="number">1</li>
+      <!-- 第一页 -->
+      <!-- 如果当前页数为1，那么样式为显蓝色，且按钮为禁用 -->
+      <!-- 总页数大于0的时候才会显示哦 -->
     <li
       class="el-icon more btn-quickprev"
       :class="[quickprevIconClass, { disabled }]"
       v-if="showPrevMore"
       @mouseenter="onMouseenter('left')"
       @mouseleave="quickprevIconClass = 'el-icon-more'">
+      <!-- 显示向左更多 没有移上去就是...，移上去就是一个向左的图标 -->
+      <!-- quickprevIconClass ...的图标 -->
+      <!-- showPrevMore 这个为真的时候才显示，因为有时候页数没达到那么多，...就不会显示，因此下面需要判断 -->
+      <!-- 两个移入移除的操作就是样式改变了 -->
     </li>
     <li
       v-for="pager in pagers"
       :key="pager"
       :class="{ active: currentPage === pager, disabled }"
       class="number">{{ pager }}</li>
+    <!-- 控制要显示哪些页数 - 页码 -->
+    <!-- pager是一个计算属性 -->
     <li
       class="el-icon more btn-quicknext"
       :class="[quicknextIconClass, { disabled }]"
       v-if="showNextMore"
       @mouseenter="onMouseenter('right')"
       @mouseleave="quicknextIconClass = 'el-icon-more'">
+      <!-- 向右更多 没有移上去就是...，移上去就是一个向右的图标 -->
     </li>
     <li
       :class="{ active: currentPage === pageCount, disabled }"
       class="number"
       v-if="pageCount > 1">{{ pageCount }}</li>
+      <!-- 总页数 -->
   </ul>
 </template>
 
@@ -35,40 +46,47 @@
     name: 'ElPager',
 
     props: {
+      // 当前页数，支持 .sync 修饰符
       currentPage: Number,
-
+      // 总页数，total 和 page-count 设置任意一个就可以达到显示页码的功能；如果要支持 page-sizes 的更改，则需要使用 total 属性
       pageCount: Number,
-
+      // 页码按钮的数量，当总页数超过该值时会折叠    大于等于 5 且小于等于 21 的奇数    7
       pagerCount: Number,
-
+      // 是否禁用
       disabled: Boolean
     },
 
     watch: {
+      // 是否显示<<     el-icon-more对应的是...的字体图标
       showPrevMore(val) {
         if (!val) this.quickprevIconClass = 'el-icon-more';
       },
-
+      // 是否显示>>
       showNextMore(val) {
         if (!val) this.quicknextIconClass = 'el-icon-more';
       }
     },
 
     methods: {
+      // 方法被绑定在ul上，而不是li标签上，使用到的是事件代理模式
       onPagerClick(event) {
         const target = event.target;
         if (target.tagName === 'UL' || this.disabled) {
           return;
         }
-
+        // 找到点击对象的内容
         let newPage = Number(event.target.textContent);
+        // 带有数字的按钮数
         const pageCount = this.pageCount;
         const currentPage = this.currentPage;
+        // 每次移动的距离
         const pagerCountOffset = this.pagerCount - 2;
-
+        // 点击...
         if (target.className.indexOf('more') !== -1) {
+          // 点击<<
           if (target.className.indexOf('quickprev') !== -1) {
             newPage = currentPage - pagerCountOffset;
+            // 点击>>
           } else if (target.className.indexOf('quicknext') !== -1) {
             newPage = currentPage + pagerCountOffset;
           }
@@ -76,55 +94,66 @@
 
         /* istanbul ignore if */
         if (!isNaN(newPage)) {
+          // 最小为1
           if (newPage < 1) {
             newPage = 1;
           }
-
+          // 最大为pageCount
           if (newPage > pageCount) {
             newPage = pageCount;
           }
         }
-
+        // 新旧值不相等
         if (newPage !== currentPage) {
           this.$emit('change', newPage);
         }
       },
-
+      // 鼠标移入
       onMouseenter(direction) {
         if (this.disabled) return;
         if (direction === 'left') {
+          // 设置<<类名
           this.quickprevIconClass = 'el-icon-d-arrow-left';
         } else {
+          // 设置>>类名
           this.quicknextIconClass = 'el-icon-d-arrow-right';
         }
       }
     },
 
     computed: {
+      // example < 1 ... 3 4 5 6 7 8 9 10 11 ... 50 >  currentPage  = 7
       pagers() {
-        const pagerCount = this.pagerCount;
+        const pagerCount = this.pagerCount;  //  11 猜测一共显示11个数字，不包括...以及上下一页
         const halfPagerCount = (pagerCount - 1) / 2;
 
         const currentPage = Number(this.currentPage);
+        // 总页数
         const pageCount = Number(this.pageCount);
 
         let showPrevMore = false;
         let showNextMore = false;
 
-        if (pageCount > pagerCount) {
+        // 判断是否有more
+        if (pageCount > pagerCount) {// 50 > 11
           if (currentPage > pagerCount - halfPagerCount) {
+            // 7 > 11 - 5
             showPrevMore = true;
           }
 
           if (currentPage < pageCount - halfPagerCount) {
+            // 7 < 50 - 5
             showNextMore = true;
           }
         }
 
         const array = [];
 
+         // 如果有more的话，具体是四种情况的哪一种
         if (showPrevMore && !showNextMore) {
+          //  41 = 50 - (11 - 2)
           const startPage = pageCount - (pagerCount - 2);
+          // pageCount 50
           for (let i = startPage; i < pageCount; i++) {
             array.push(i);
           }
@@ -133,7 +162,9 @@
             array.push(i);
           }
         } else if (showPrevMore && showNextMore) {
+          // offset 4
           const offset = Math.floor(pagerCount / 2) - 1;
+          // for (let i = 3;i <= 11;i++)
           for (let i = currentPage - offset ; i <= currentPage + offset; i++) {
             array.push(i);
           }
--- packages/pagination/src/pagination.js
+++ packages/pagination/src/pagination.js
@@ -9,17 +9,18 @@ export default {
   name: 'ElPagination',
 
   props: {
+    //每页显示条目个数，支持 .sync 修饰符
     pageSize: {
       type: Number,
       default: 10
     },
-
+    //是否使用小型分页样式
     small: Boolean,
-
+    // 总条目数
     total: Number,
-
+   //总页数，total 和 page-count 设置任意一个就可以达到显示页码的功能；如果要支持 page-sizes 的更改，则需要使用 total 属性
     pageCount: Number,
-
+   //页码按钮的数量，当总页数超过该值时会折叠
     pagerCount: {
       type: Number,
       validator(value) {
@@ -27,62 +28,69 @@ export default {
       },
       default: 7
     },
-
+    // 当前页数，支持 .sync 修饰符
     currentPage: {
       type: Number,
       default: 1
     },
-
+    //组件布局，子组件名用逗号分隔
     layout: {
       default: 'prev, pager, next, jumper, ->, total'
     },
-
+    //每页显示个数选择器的选项设置
     pageSizes: {
       type: Array,
       default() {
         return [10, 20, 30, 40, 50, 100];
       }
     },
-
+    // 每页显示个数选择器的下拉框类名
     popperClass: String,
-
+    // 替代图标显示的上一页文字
     prevText: String,
-
+    // 替代图标显示的下一页文字
     nextText: String,
-
+    // 是否为分页按钮添加背景色
     background: Boolean,
-
+    // 是否禁用
     disabled: Boolean,
-
+    //只有一页时是否隐藏
     hideOnSinglePage: Boolean
   },
 
   data() {
     return {
+      // 当前的页码
       internalCurrentPage: 1,
+      // 总页数 对这个注释保持怀疑态度
       internalPageSize: 0,
       lastEmittedPage: -1,
       userChangePageSize: false
     };
   },
-
+  // render函数生成el-pagination
   render(h) {
+    //this.layout就是之前我们在props里面定义的组件布局，如果每页子组件那么就显示null
     const layout = this.layout;
     if (!layout) return null;
+    //处理只有一页是否隐藏
     if (this.hideOnSinglePage && (!this.internalPageCount || this.internalPageCount === 1)) return null;
 
+    // 最外层的div template 是一个父容器模板，定义的时候可以先处理它的样式，包括背景以及小样式设定
     let template = <div class={['el-pagination', {
       'is-background': this.background,
       'el-pagination--small': this.small
     }] }></div>;
+
+    //TEMPLATE_MAP 是一个组件集合，里面涉及的组件会在后面定义
     const TEMPLATE_MAP = {
-      prev: <prev></prev>,
-      jumper: <jumper></jumper>,
+      prev: <prev></prev>,// 上一页
+      jumper: <jumper></jumper>,// 跳转 前往多少页
       pager: <pager currentPage={ this.internalCurrentPage } pageCount={ this.internalPageCount } pagerCount={ this.pagerCount } on-change={ this.handleCurrentChange } disabled={ this.disabled }></pager>,
-      next: <next></next>,
-      sizes: <sizes pageSizes={ this.pageSizes }></sizes>,
+      next: <next></next>,// 下一页
+      sizes: <sizes pageSizes={ this.pageSizes }></sizes>,// 每页显示条目个数
       slot: <slot>{ this.$slots.default ? this.$slots.default : '' }</slot>,
-      total: <total></total>
+      total: <total></total>// 总共的页数
     };
     const components = layout.split(',').map((item) => item.trim());
     const rightWrapper = <div class="el-pagination__rightwrapper"></div>;
@@ -90,12 +98,14 @@ export default {
 
     template.children = template.children || [];
     rightWrapper.children = rightWrapper.children || [];
+    //  // ->这个符号主要是将其后面的组件放在rightWrapper中，然后右浮动；如果存在->符号，就将haveRightWrapper为true
     components.forEach(compo => {
       if (compo === '->') {
         haveRightWrapper = true;
         return;
       }
 
+      // 当haveRightWrapper为true,即在->后面的放入rightWrapper中
       if (!haveRightWrapper) {
         template.children.push(TEMPLATE_MAP[compo]);
       } else {
@@ -112,6 +122,7 @@ export default {
 
   components: {
     Prev: {
+      // 上一页组件 就是点击'上一页'或者前进按钮 (以及边界处理)  prevText用户设置的替代上一页图标的文字，存在显示文字，不存在显示上一页图标
       render(h) {
         return (
           <button
@@ -128,7 +139,7 @@ export default {
         );
       }
     },
-
+   // 下一页组件 当前页数等于总页数时 或者 总页数等于0时，下一页按钮被禁用
     Next: {
       render(h) {
         return (
@@ -146,19 +157,22 @@ export default {
         );
       }
     },
-
+  // 每页显示条目个数组件
     Sizes: {
       mixins: [Locale],
 
-      props: {
-        pageSizes: Array
+      props: { 
+        pageSizes: Array  // 每页显示个数选择器的选项设置   [10, 20, 30, 40, 50 ]
       },
 
       watch: {
+        // 声明了之后会立马执行handler里面的函数
         pageSizes: {
           immediate: true,
           handler(newVal, oldVal) {
             if (valueEquals(newVal, oldVal)) return;
+            // 如果用户设置了每页显示的条目个数，并且pageSize在设置的pageSizes中存在的话，就显示pageSize，否则就显示this.pageSizes[0]
+            // 最后将每页显示的条目个数赋值给this.$parent.internalPageSize
             if (Array.isArray(newVal)) {
               this.$parent.internalPageSize = newVal.indexOf(this.$parent.pageSize) > -1
                 ? this.$parent.pageSize
@@ -169,7 +183,7 @@ export default {
       },
 
       render(h) {
-        return (
+        return (// 渲染出每页可选的分页数   this.t('el.pagination.pagesize') 返回'条/页'
           <span class="el-pagination__sizes">
             <el-select
               value={ this.$parent.internalPageSize }
@@ -200,13 +214,15 @@ export default {
           if (val !== this.$parent.internalPageSize) {
             this.$parent.internalPageSize = val = parseInt(val, 10);
             this.$parent.userChangePageSize = true;
+            //如果父组件中pageSize用了.sync 修饰符，这里将会触发父组件的update，改变pageSize的值
             this.$parent.$emit('update:pageSize', val);
+            //触发父组件的size-change事件，将改变的每页显示的条目个数的值传递出去
             this.$parent.$emit('size-change', val);
           }
         }
       }
     },
-
+    // 前往多少页组件
     Jumper: {
       mixins: [Locale],
 
@@ -225,6 +241,7 @@ export default {
       },
 
       methods: {
+        // 按下回车，前往多少页
         handleKeyup({ keyCode, target }) {
           // Chrome, Safari, Firefox triggers change event on Enter
           // Hack for IE: https://github.com/ElemeFE/element/issues/11710
@@ -236,13 +253,15 @@ export default {
         handleInput(value) {
           this.userInput = value;
         },
+        // 改变当前页
         handleChange(value) {
+          // 更新页码列表中当前页的值
           this.$parent.internalCurrentPage = this.$parent.getValidCurrentPage(value);
           this.$parent.emitChange();
           this.userInput = null;
         }
       },
-
+      // 前往多少页
       render(h) {
         return (
           <span class="el-pagination__jump">
@@ -262,7 +281,7 @@ export default {
         );
       }
     },
-
+    // 总共的页数，组件
     Total: {
       mixins: [Locale],
 
@@ -282,6 +301,7 @@ export default {
     handleCurrentChange(val) {
       this.internalCurrentPage = this.getValidCurrentPage(val);
       this.userChangePageSize = true;
+      // 触发父组件current-change事件，并传递相应的值
       this.emitChange();
     },
 
@@ -289,7 +309,10 @@ export default {
       if (this.disabled) return;
       const newVal = this.internalCurrentPage - 1;
       this.internalCurrentPage = this.getValidCurrentPage(newVal);
+      // 用户点击上一页按钮改变当前页后触发    当前页
+      // 触发父组件的prev-click事件，并将CurrentPage传递出去
       this.$emit('prev-click', this.internalCurrentPage);
+      // 触发父组件current-change事件，并传递相应的值
       this.emitChange();
     },
 
@@ -297,10 +320,12 @@ export default {
       if (this.disabled) return;
       const newVal = this.internalCurrentPage + 1;
       this.internalCurrentPage = this.getValidCurrentPage(newVal);
+      // 用户点击下一页按钮改变当前页后触发    当前页
+      // 触发父组件的next-click事件，并将CurrentPage传递出去
       this.$emit('next-click', this.internalCurrentPage);
       this.emitChange();
     },
-
+    // 校验需要前往的页码的值
     getValidCurrentPage(value) {
       value = parseInt(value, 10);
 
@@ -310,13 +335,14 @@ export default {
       if (!havePageCount) {
         if (isNaN(value) || value < 1) resetValue = 1;
       } else {
+        // 如果当前页码小于1，则取1；如果当前页码大于最大页码，则取最大页码
         if (value < 1) {
           resetValue = 1;
         } else if (value > this.internalPageCount) {
           resetValue = this.internalPageCount;
         }
       }
-
+      // 如果当前页码是非数字，或者为0，则将当前页码置为1，并返回
       if (resetValue === undefined && isNaN(value)) {
         resetValue = 1;
       } else if (resetValue === 0) {
@@ -328,8 +354,12 @@ export default {
 
     emitChange() {
       this.$nextTick(() => {
+        // 用户改变当前PageSize时，触发父组件的current-change事件
         if (this.internalCurrentPage !== this.lastEmittedPage || this.userChangePageSize) {
+          // currentPage 改变时会触发    当前页
           this.$emit('current-change', this.internalCurrentPage);
+          // 当前页赋值给上次点击的页面
+          // lastEmittedPage记录最后传递的CurrentPage的值
           this.lastEmittedPage = this.internalCurrentPage;
           this.userChangePageSize = false;
         }
@@ -338,10 +368,13 @@ export default {
   },
 
   computed: {
+    // 总页码
     internalPageCount() {
       if (typeof this.total === 'number') {
+        // 总页数 = 总条目数 / 每页的显示条数
         return Math.max(1, Math.ceil(this.total / this.internalPageSize));
       } else if (typeof this.pageCount === 'number') {
+        // 总页数
         return Math.max(1, this.pageCount);
       }
       return null;
@@ -362,7 +395,7 @@ export default {
         this.internalPageSize = isNaN(val) ? 10 : val;
       }
     },
-
+    // internalCurrentPage改变时去触发父组件中currentPage更新
     internalCurrentPage: {
       immediate: true,
       handler(newVal) {
@@ -370,7 +403,7 @@ export default {
         this.lastEmittedPage = -1;
       }
     },
-
+    // 总页码
     internalPageCount(newVal) {
       /* istanbul ignore if */
       const oldPage = this.internalCurrentPage;
